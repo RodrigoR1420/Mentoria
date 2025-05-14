7ad8c915606a80c124e3830790b7c0593fc9dfd4
Rodrigo Lima Rodrigues <rodrigaolima11@gmail.com>
2025-05-13 21:33:28 -0300

    commit

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -13,3 +13,35 @@ function clearDisplay(){
 function calculate(){
     display.value = eval(display.value)
 }
+
+function convertTime(unit) {
+    let value = parseFloat(display.value);
+
+    if (isNaN(value)) {
+        alert("Digite um número válido antes de converter!");
+        return;
+    }
+
+    let result;
+
+    switch(unit) {
+        case 'days':
+            result = value / 24;
+            display.value = `${result} dia(s)`;
+            break;
+        case 'hours':
+            result = value * 24;
+            display.value = `${result} hora(s)`;
+            break;
+        case 'minutes':
+            result = value * 24 * 60;
+            display.value = `${result} minuto(s)`;
+            break;
+        case 'seconds':
+            result = value * 24 * 60 * 60;
+            display.value = `${result} segundo(s)`;
+            break;
+        default:
+            alert("Unidade desconhecida.");
+    }
+}
---
+++ calculator/lucas.js
@@ -0,0 +1,8 @@
+const number = 5
+
+if (number % 2 == 0){
+    console.log('é par!')
+}
+else{
+    console.log(`é ímpar!`)
+}
