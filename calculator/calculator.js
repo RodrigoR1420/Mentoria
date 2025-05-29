4e875bcc0c05d0d6afdeb11212c73aab9c767e69
Rodrigo Lima Rodrigues <rodrigaolima11@gmail.com>
2025-05-29 15:58:32 -0300

    c

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,47 +1,135 @@
 
 
-const display = document.querySelector('#display');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const display = document.getElementById("display");
+let tempo = 'days'
 
 function appendToDisplay(input){
-    display.value += input;
+    display.value += input
 }
 
-function clearDisplay(){
-    display.value = '';
+function addChar(unit) {
+    let value = display.value;
+    const match = value.match(/(\d+)(?![dhms])$/)
+
+    if (match) {
+        display.value = value.slice(0, -match[0].length) + match[0] + unit
+    }
 }
 
-function calculate(){
-    display.value = eval(display.value)
+function clearDisplay() {
+    display.value = ''
 }
 
 function convertTime(unit) {
-    let value = parseFloat(display.value);
+    tempo = unit
+}
 
-    if (isNaN(value)) {
-        alert("Digite um número válido antes de converter!");
-        return;
+function calculate() {
+    const input = display.value
+    const regex = /(\d+)([dhms])/g
+    let match;
+    let totalSeconds = 0
+
+    while ((match = regex.exec(input)) !== null) {
+        const value = +match[1]
+        const unit = match[2]
+
+        switch (unit) {
+            case 'd':
+                totalSeconds += value * 86400;
+                break;
+            case 'h':
+                totalSeconds += value * 3600;
+                break;
+            case 'm':
+                totalSeconds += value * 60;
+                break;
+            case 's':
+                totalSeconds += value;
+                break;
+        }
     }
 
-    let result;
-
-    switch(unit) {
+    let result
+    switch (targetUnit) {
         case 'days':
-            result = value / 24;
-            display.value = `${result} dia(s)`;
+            result = totalSeconds / 86400;
+            display.value = `${result} dia(s)`
             break;
         case 'hours':
-            result = value * 24;
-            display.value = `${result} hora(s)`;
+            result = totalSeconds / 3600;
+            display.value = `${result} hora(s)`
             break;
         case 'minutes':
-            result = value * 24 * 60;
-            display.value = `${result} minuto(s)`;
+            result = totalSeconds / 60;
+            display.value = `${result} minuto(s)`
             break;
         case 'seconds':
-            result = value * 24 * 60 * 60;
-            display.value = `${result} segundo(s)`;
-            break;
+            result = totalSeconds;
+            display.value = `${result} segundo(s)`
+            break
         default:
-            alert("Unidade desconhecida.");
+            alert("Unidade desconhecida.")
     }
 }
+*/
+
